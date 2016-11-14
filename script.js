6167f2e099f8e7261a5c2918f63f3cd5c859181d
Matt Lubel <matt_lubel@hotmail.com>
2016-11-14 10:51:38 +0000

    Tidied a bit. Still WIP

--- script.js
+++ script.js
@@ -1,143 +1,139 @@
-var get_word = function () {
-	
-	var request = new XMLHttpRequest;
-	request.open('GET', 'https://crossorigin.me/http://randomword.setgetgo.com/get.php');
-	request.onload = function() {
-		if (request.status == 200){		
-			document.getElementById("word").innerHTML = request.response;
-		}
-	};
-	
-	request.onerror = function() {
-		console.log('connection error');
-	};
-	
-	request.send();		
-};
-
-// add button click event
-// document.getElementById("button").addEventListener("click", get_word);
-
 var HangmanApp = function(elem) {
 
 	var alphabet = "abcdefghijklmnopqrstuvwxyz",
-		word = "test",
+		request,
+		word = "testosterone",
 		word_length,
-		letters_guessed = ["e"],
+		letters_guessed = [],
 		
-		displayed_word = "test",
+		displayed_word,
   		lives_left = 5,
+  		game_complete = false;
 		
-		// DOM elements
-		top_display,
-  		DOM_displayed_word,
-  		DOM_lives_left,
-  		DOM_letters_guessed,
-  		buttons_section,
+	// create DOM elements
+	var top_display = quickCreateElement("div", "top-display"),  	
+		DOM_displayed_word = quickCreateElement("div", "displayed-word"),
+		DOM_lives_left = quickCreateElement("div", "lives-left"),
+		DOM_game_message = quickCreateElement("div", "message"),
+		buttons_section = quickCreateElement("div", "buttons-section"),
+  		// DOM_letters_guessed,
 		letter_buttons = [];	
   		
-  	
-  	// create DOM elements
-  	top_display = document.createElement("div");
-  	top_display.classList.add("top-display");
-  	
-  	DOM_displayed_word = document.createElement("span"); 
-  	top_display.appendChild(DOM_displayed_word);
-  		
-  	DOM_lives_left = document.createElement("span");
-  	top_display.appendChild(DOM_lives_left);
-  	
-  	buttons_section = document.createElement("div");
-  	buttons_section.classList.add("buttons-section");
-  	
   	// create buttons
   	for (var i=0; i<26; i++) {
-  		letter_buttons.push(createButton(alphabet[i]));
+  		letter_buttons.push(quickCreateElement("button", "letter-button", alphabet[i]));
   	}
   	
-	// initial DOM display    
-	elem.appendChild(top_display);
-	elem.appendChild(buttons_section);
-	
+  	// organise DOM elements	  	
+  	top_display.appendChild(DOM_displayed_word);
+  	top_display.appendChild(DOM_lives_left);
+  	top_display.appendChild(DOM_game_message);
+  	
 	for (var i = 0; i < 26; i++) {
 		buttons_section.appendChild(letter_buttons[i]);
 	}
-
+  	
+  	
+  	// HELPER FUNCTIONS
+  	
+  	function quickCreateElement(type, cls, id) {
+  		var ret = document.createElement(type);
+  		if (cls) { ret.classList.add(cls); }
+  		if (id) { ret.id = id; }
+  		return ret
+  	}
+  		
 	function contains(arr, el) {
+	// function to check if arr contains el
 		for (var i = 0; i < arr.length; i++) {
 			if (arr[i] == el) { return true }
 		}
 		return false
 	};
 
-	// private functions
+
+	// PROCESS FUNCTIONS
+	
+	var getWord = function () {
+		request = new XMLHttpRequest;
+		request.open('GET', 'https://crossorigin.me/http://randomword.setgetgo.com/get.php');
+		request.onload = function() {
+			if (request.status == 200){		
+				word = request.response;
+			}
+			// ...else
+		};
+		request.onerror = function() {
+			console.log('connection error');
+		};
+		request.send();		
+	};
+	
+	function loadInitialDOM() {    
+		elem.appendChild(top_display);
+		elem.appendChild(buttons_section);
+	}
+
+	function render() {
+		renderDisplayedWord();
+		DOM_lives_left.innerHTML = "Lives left: " + lives_left;
+		evaluateResult();
+		if (game_complete) {
+			DOM_game_message.innerHTML = game_complete;
+		}
+		renderButtons(game_complete);
+	};
+
 	function renderDisplayedWord() {
-		displayed_word = ""
+		displayed_word = "";
 		for (var i = 0; i < word.length; i++) {
-			// var guessed = false;
-// 			for (var j = 0; j < letters_guessed.length; j++) {
-// 				if (word[i] == letters_guessed[j]){
-// 					displayed_word += word[i] + " ";
-// 					guessed = true;
-// 				}
-// 			}
-// 			if (!guessed) {
-// 				displayed_word += "_ ";
-// 			}
 			if (contains(letters_guessed, word[i])) {
-				displayed_word += word[i] + " ";
+				displayed_word += word[i];
 			}
 			else {
-				displayed_word += "_ ";
+				displayed_word += "_";
 			}
+			displayed_word += (i == word.length) ? "" : " " ;
 		}
 		DOM_displayed_word.innerHTML = displayed_word;
 	};
-		
-	function render() {
-		renderDisplayedWord();
-		DOM_lives_left.innerHTML = lives_left;
-		renderButtons();
-	};
 	
-	function startGame() {
-		// get word
-		render();
-		
+	function evaluateResult() {
+		if (!contains(displayed_word, "_")) {
+			game_complete = "Congratulations! You win!";
+		}
+		if (lives_left <= 0) {
+			game_complete = "Bad luck, you lose! The correct word was " + word;
+		}
 	}
 
-	function renderButtons() {
+	function renderButtons(game_over) {
 		for (var i = 0; i < 26; i++) {
-			
-			
+			b = letter_buttons[i];
+			b.innerHTML = "";
+			b.removeEventListener("click", letter_select);
+			if (!game_over && !contains(letters_guessed, alphabet[i])) {
+				b.innerHTML = alphabet[i];
+				b.addEventListener("click", letter_select);
+			}
 		}
 	};
-
-	function createButton(letter) {
-		var b = document.createElement("button");
-		b.id = letter;
-		b.innerHTML = letter;
-		b.addEventListener("click", letter_select);
-		return b;
-	}
   
 	function letter_select() {
-		var letter = event.target.id,
-			correct_guess = false;
-		// if not already there...
+		var letter = event.target.id;
+		// if not already there..
 		letters_guessed.push(letter);
-		for (var i = 0; i < word.length; i++)
-			if (letter == word[i]) {
-				correct_guess = true;
-				break;
-			}
-		if (!correct_guess) {
+		if (!contains(word, letter)) {
 			lives_left -= 1;
 		}
 		render();
 	}
 
-	startGame();
+	// START GAME (was previous abstracted into startGame() function)
+	
+	// getWord();
+	loadInitialDOM();
+	render();	
 
 };
 
